cffbf1049ef33e2cab6ff1e42ce1c000f9708018
Krishna Chaitanya <dkchaitanyavarma06@gmail.com>
2021-02-28 15:22:18 +0530

    Adding Logic to Delete Items

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -11,7 +11,7 @@ const CartItem = props => {
       </View>
       <View style={styles.itemData}>
         <Text style={styles.mainText}>${props.amount.toFixed(2)}</Text>
-        <TouchableOpacity style={styles.deleteButton}>
+        <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
           <Ionicons
             name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
             size={23}
