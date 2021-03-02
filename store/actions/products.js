52d68a89c963089d66b2dbcb8ee74943ad55b977
Krishna Chaitanya <dkchaitanyavarma06@gmail.com>
2021-03-02 16:50:10 +0530

    Fetching Products from Server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,33 @@
+import Product from '../../models/product';
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    const response = await fetch('https://react-native-shop-app-1454d-default-rtdb.firebaseio.com/products.json');
+
+    const resData = await response.json();
+    const loadedProducts = [];
+
+    for (const key in resData) {
+      loadedProducts.push(
+        new Product(
+          key,
+          'u1',
+          resData[key].title,
+          resData[key].imageUrl,
+          resData[key].description,
+          resData[key].price
+        )
+      );
+    }
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  };
+};
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
@@ -22,8 +49,6 @@ export const createProduct = (title, description, imageUrl, price) => {
     });
 
     const resData = await response.json();
-
-    console.log(resData);
 
     dispatch({
       type: CREATE_PRODUCT,
