d3dbe81f046df81eb1aa316129b74860c87d39ab
Krishna Chaitanya <dkchaitanyavarma06@gmail.com>
2021-03-02 18:45:00 +0530

    Handling Errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -39,12 +39,16 @@ export const fetchProducts = () => {
 
 export const deleteProduct = productId => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://react-native-shop-app-1454d-default-rtdb.firebaseio.com/products/${productId}.json`,
       {
         method: 'DELETE',
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
@@ -82,7 +86,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async dispatch => {
-    await fetch(
+    const response = await fetch(
       `https://react-native-shop-app-1454d-default-rtdb.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -96,6 +100,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         })
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
